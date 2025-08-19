c6145b3a3d587ab2e0537d24758c0efd219b1b1f
Shashwat Tyagi <shashwattyagi988@gmail.com>
2025-08-19 11:59:25 +0530

    project completed

---
+++ app/(main)/doctor/layout.jsx
@@ -0,0 +1,17 @@
+import { PageHeader } from "@/components/page-header";
+import { Stethoscope } from "lucide-react";
+
+export const metadata = {
+  title: "Doctor Dashboard - Healthease",
+  description: "Manage your appointments and availability",
+};
+
+export default async function DoctorDashboardLayout({ children }) {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <PageHeader icon={<Stethoscope />} title="Doctor Dashboard" />
+
+      {children}
+    </div>
+  );
+}
---
+++ app/(main)/doctors/[specialty]/[id]/page.jsx
@@ -0,0 +1,26 @@
+import { getAvailableTimeSlots, getDoctorById } from "@/actions/appointments";
+import { redirect } from "next/navigation";
+import { DoctorProfile } from "./components/doctor-profile";
+
+export default async function DoctorProfilePage({ params }) {
+  const { id } = await params;
+
+  try {
+    // Fetch doctor data and available slots in parallel
+    const [doctorData, slotsData] = await Promise.all([
+      getDoctorById(id),
+      getAvailableTimeSlots(id),
+    ]);
+    console.log(doctorData)
+
+    return (
+      <DoctorProfile
+        doctor={doctorData.doctor}
+        availableDays={slotsData.days || []}
+      />
+    );
+  } catch (error) {
+    console.error("Error loading doctor profile:", error);
+    redirect("/doctors");
+  }
+}
